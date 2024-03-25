095acdcbe1bd1f1377c11b10f3250da2b2f7deaf
fred2020nic <fep.200978@gmail.com>
2024-03-25 11:42:35 -0600

    prueba deploy

--- admin/home.php
+++ admin/home.php
@@ -23,7 +23,7 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
-        <li class="active">Panel de Control</li>
+        <li class="active">Panel de Control 2024 - deploy git</li>
       </ol>
     </section>
 
